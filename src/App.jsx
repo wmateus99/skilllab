34566e00a49615057314ce7ff0a537ae617b41fe
wmateus99 <wandersonmateus07@gmail.com>
2025-11-24 11:53:59 -0300

    Update (Página Inicial)

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,90 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles/styl-content-home.css"
+import isologo from './assets/isologo.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <section className="content-home">
+                <header>
+                    <img src={isologo} alt="Logo" />
+                </header>
+                <div className="container">
+                    <main className="box-main">
+                        <div className="box-title">
+                            <h1>
+                                SEU AMBIENTE DE <br/> PRÁTICA E APRENDIZADO
+                            </h1>
+                            <p>
+                                Um espaço dedicado para você aprender, praticar e desenvolver suas habilidades com apoio e recursos ideais.
+                            </p>
+                        </div>
+                    </main>
+                    <div className="box-navegation">
+                        <div className="box-itens">
+                            <div className="item">
+                                <div className="box-item-title">
+                                    <h2>PRIMEIRA AULA</h2>
+                                </div>
+                                <div className="box-item-description">
+                                    <div className="line"></div>
+                                    <p>
+                                        Um espaço dedicado para você aprender, praticar e desenvolver suas habilidades com apoio e recursos ideais.
+                                    </p>
+                                </div>
+                                <div className="box-item-buttons">
+                                    <button>Acessar</button>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="box-item-title">
+                                    <h2>PRIMEIRA AULA</h2>
+                                </div>
+                                <div className="box-item-description">
+                                    <div className="line"></div>
+                                    <p>
+                                        Um espaço dedicado para você aprender, praticar e desenvolver suas habilidades com apoio e recursos ideais.
+                                    </p>
+                                </div>
+                                <div className="box-item-buttons">
+                                    <button>Acessar</button>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="box-item-title">
+                                    <h2>PRIMEIRA AULA</h2>
+                                </div>
+                                <div className="box-item-description">
+                                    <div className="line"></div>
+                                    <p>
+                                        Um espaço dedicado para você aprender, praticar e 
+                                    </p>
+                                </div>
+                                <div className="box-item-buttons">
+                                    <button>Acessar</button>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="box-item-title">
+                                    <h2>PRIMEIRA AULA</h2>
+                                </div>
+                                <div className="box-item-description">
+                                    <div className="line"></div>
+                                    <p>
+                                        Um espaço dedicado para você aprender, praticar e desenvolver suas habilidades com apoio e recursos ideais.
+                                    </p>
+                                </div>
+                                <div className="box-item-buttons">
+                                    <button>Acessar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import BgGradient from "./components/BgGradient.jsx";
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/teste",
+    element: <h1>Ola</h1>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <BgGradient/> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
